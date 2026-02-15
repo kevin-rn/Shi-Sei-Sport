8c169d671d7cce1798b0c7f4c80c9864c25e57b3
Kevin Nanhekhan <kevin.nanhekhan@outlook.com>
2026-02-15 23:35:31 +0100

    (fix): remove migration

--- backend/src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20260215_221251___name from './20260215_221251___name';
-
-export const migrations = [
-  {
-    up: migration_20260215_221251___name.up,
-    down: migration_20260215_221251___name.down,
-    name: '20260215_221251___name'
-  },
-];
